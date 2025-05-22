50305ea8314ba196e4981cc9663a655b48152cdf
Mario Auliarahman <marioauliarahman@gmail.com>
2025-05-22 21:56:40 +0700

    Update aplikasiAnalisisSentimen.go

--- aplikasiAnalisisSentimen.go
+++ aplikasiAnalisisSentimen.go
@@ -18,7 +18,7 @@ var daftarKomentar [NMAX]komentar
 var jumlahKomentar int
 
 // Daftar kata sentimen
-var komentarPositif = [NMAX]string{"baik", "bagus", "mantap", "hebat", "luar biasa", "puas", "senang", "terbaik", "positif", "wow", "suka", "menakjubkan", "inspiratif", "bermanfaat"}
+var komentarPositif = [NMAX]string{"baik", "bagus", "mantap", "hebat", "luar biasa", "puas", "senang", "terbaik", "positif", "wow", "suka", "menakjubkan", "inspiratif", "bermanfaat", "keren"}
 var komentarNegatif = [NMAX]string{"jelek", "buruk", "parah", "bodoh", "jijik", "minus", "negatif", "bohong", "menyesal", "gila", "tidak puas", "jelek banget", "benci", "tidak bermanfaat", "mati"}
 
 // fungsi untuk menganalisa sentimen berdasarkan teks komentar
